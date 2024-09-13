c549866e9c78e64af1b4463b6d9859e42ba64ca9
S1mple757 <166694874+S1mple757@users.noreply.github.com>
2024-09-13 13:52:10 +0300

    Create ChangeWallpaper_v2.ps1

---
+++ ChangeWallpaper_v2.ps1
@@ -0,0 +1,20 @@
+# Path to the wallpaper image
+$imagePath = "C:\temp\Wallp\ver01\polina-rytova-1dGMs4hhcVA-unsplash.jpg"
+
+# Define the Windows API function to set the wallpaper
+Add-Type -TypeDefinition @"
+using System;
+using System.Runtime.InteropServices;
+
+public class Wallpaper {
+    [DllImport("user32.dll", CharSet = CharSet.Auto)]
+    public static extern int SystemParametersInfo(int uAction, int uParam, string lpvParam, int fuWinIni);
+}
+"@
+
+# Constants for SystemParametersInfo function
+$SPI_SETDESKWALLPAPER = 0x0014
+$SPIF_UPDATEINIFILE = 0x0001
+
+# Call the SystemParametersInfo function to set the wallpaper
+[Wallpaper]::SystemParametersInfo($SPI_SETDESKWALLPAPER, 0, $imagePath, $SPIF_UPDATEINIFILE)
